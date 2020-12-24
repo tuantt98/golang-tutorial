1c1556f6ea1cafd560b240ca0c0cc664d6e3bde0
kuro <kisuvitinh@gmail.com>
2020-12-24 09:51:29 +0700

    method

---
+++ 12-method/main.go
@@ -0,0 +1,36 @@
+// func => 1 chức năng riêng
+// method => thuộc về đối tượng nào
+
+package main
+
+import "fmt"
+
+type Student struct {
+	name string
+}
+
+func (s Student) getName() string {
+	return s.name
+}
+
+// value receiver
+// => không làm thay đổi giá trị
+// vì nó tạo ra 1 bãn copy và thay đổi trên đó
+func (s Student) changeName() {
+	s.name = "kazuto"
+}
+
+// pointer receiver
+func (s *Student) changeName2() {
+	s.name = "kazuto"
+}
+
+func main() {
+	st1 := &Student{name: "kuro"}
+	// name := st1.getName()
+	st1.changeName()
+	fmt.Println(st1.name)
+
+	st1.changeName2()
+	fmt.Println(st1.name)
+}
